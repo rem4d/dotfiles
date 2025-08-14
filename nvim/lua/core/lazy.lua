00b7ed295537d3746e7702d8fc56a08d94b0798c
Jane <bleazint@gmail.com>
2025-08-14 23:14:23 +0300

    Remove user dir

--- nvim/lua/core/lazy.lua
+++ nvim/lua/core/lazy.lua
@@ -11,7 +11,7 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup({ { import = "user.plugins" }, { import = "user.plugins.lsp" } }, {
+require("lazy").setup({ { import = "plugins" } }, {
 	install = {
 		colorscheme = { "sonokai" },
 		-- colorscheme = { "tokyonight" },
---
+++ nvim/lua/core/lsp.lua
@@ -0,0 +1,20 @@
+vim.lsp.enable({
+	"vtsls",
+})
+
+vim.diagnostic.config({
+	virtual_lines = true,
+	-- virtual_text = true,
+	float = {
+		border = "rounded",
+		source = true,
+	},
+	signs = {
+		text = {
+			[vim.diagnostic.severity.ERROR] = "󰅚 ",
+			[vim.diagnostic.severity.WARN] = "󰀪 ",
+			[vim.diagnostic.severity.INFO] = "󰋽 ",
+			[vim.diagnostic.severity.HINT] = "󰌶 ",
+		},
+	},
+})
