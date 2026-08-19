42442460b7f1dfe26a21d1badb4a6f76ab155bb4
Rem <bleazint@gmail.com>
2026-08-19 13:50:22 +0300

    minor updates

--- nvim/lua/rem/init.lua
+++ nvim/lua/rem/init.lua
@@ -232,3 +232,4 @@ require("oil").setup({
 require("mini.statusline").setup()
 
 vim.keymap.set("n", "ff", function() require("fff").find_files() end, { desc = "FFFind files" })
+vim.keymap.set("n", "fg", function() require("fff").live_grep() end, { desc = "live grep" })
